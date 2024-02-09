817bb80d7331ea1477e81728622c77ba29092148
Matt Welsh <matt@fixie.ai>
2024-02-08 22:13:06 -0800

    Add longer timeout for Deepgram jobs.

--- packages/webapp/app/api/inngest/route.ts
+++ packages/webapp/app/api/inngest/route.ts
@@ -2,6 +2,9 @@ import { serve } from 'inngest/next';
 import { inngest } from '../../../inngest/client';
 import { helloWorld, processEpisodes, transcribeEpisode } from '../../../inngest/functions';
 
+// Allow max duration (can be increased on enterprise plan).
+export const maxDuration = 300;
+
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [helloWorld, processEpisodes, transcribeEpisode],
