ce1bdb93860c53e1f76caa4716772050072a0341
Matt Welsh <matt@fixie.ai>
2024-04-11 15:43:48 -0700

    Adding missing files.

---
+++ packages/utils/src/plans.ts
@@ -0,0 +1,69 @@
+/** Represents the state field of the Subscriptions table. */
+export enum SubscriptionState {
+  Active = 'active',
+  Canceled = 'canceled',
+  CancelPending = 'cancel_pending',
+}
+
+/** Represents a single plan. */
+export type Plan = {
+  id: string;
+  displayName: string;
+  description: string;
+  price: number;
+  hidden?: boolean;
+  features?: string[];
+  maxPodcasts?: number;
+  maxEpisodesPerPodcast?: number;
+  maxChatSessions?: number;
+  priceId?: string;
+};
+
+export const PLANS: Record<string, Plan> = {
+  free: {
+    id: 'free',
+    displayName: 'Free',
+    description: 'Get started for free',
+    price: 0,
+    maxPodcasts: 1,
+    maxEpisodesPerPodcast: 10,
+  },
+  creator: {
+    id: 'creator',
+    displayName: 'Creator',
+    description: 'Perfect for serious podcasters',
+    price: 15.0,
+    maxPodcasts: 3,
+    maxEpisodesPerPodcast: 50,
+    priceId: 'price_1OyzZQ1vQbZgOhBPFifGWxmL',
+  },
+  professional: {
+    id: 'professional',
+    displayName: 'Professional',
+    description: 'Unlimited power',
+    price: 50.0,
+    priceId: 'price_1OyzZw1vQbZgOhBPeqVlY4a0',
+  },
+  unlimited: {
+    id: 'unlimited',
+    displayName: 'Unlimited',
+    description: 'Unlimited everything',
+    price: 300.0,
+    hidden: true,
+  },
+};
+
+export type PodcastStat = {
+  id: number;
+  title: string;
+  description: string;
+  slug: string;
+  owner: string;
+  imageUrl: string;
+  newest: string;
+  newestprocessed: string;
+  allepisodes: number;
+  processed: number;
+  inprogress: number;
+  errors: number;
+};
