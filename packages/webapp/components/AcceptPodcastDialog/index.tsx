f6bc065002acb578e34ead15b9b5d107df0a41da
Matt Welsh <matt@fixie.ai>
2024-05-13 14:52:21 -0700

    Fix it again.

--- packages/webapp/components/AcceptPodcastDialog/index.tsx
+++ packages/webapp/components/AcceptPodcastDialog/index.tsx
@@ -35,10 +35,7 @@ export function ActivateButton({ text }: { text?: string }) {
 }
 
 export function SignUpOrActivateButton({ text, uuid }: { text?: string; uuid: string }) {
-  const redirectUrl =
-    (process.env.VERCEL_ENV === 'production' ? 'https://podverse.ai' : 'http://localhost:3000') +
-    '/activate?uuid=' +
-    uuid.replace(/-/g, '');
+  const redirectUrl = 'https://podverse.ai/activate?uuid=' + uuid.replace(/-/g, '');
   return (
     <>
       <SignedIn>
