d932ded1f560d5caf71beeeae91a62bbe7af7b00
Matt Welsh <matt@fixie.ai>
2024-06-10 13:38:01 -0700

    Adding email notifications.

--- packages/webapp/app/api/clerk-webhooks/route.ts
+++ packages/webapp/app/api/clerk-webhooks/route.ts
@@ -1,10 +1,18 @@
 import Cors from 'micro-cors';
 import { NextResponse } from 'next/server';
 import { Webhook } from 'svix';
+import { sendEmail } from '@/lib/email';
+import { clerkClient } from '@clerk/nextjs/server';
 
 const cors = Cors({
   allowMethods: ['POST', 'HEAD'],
 });
+
+const ADMIN_EMAIL = 'matt@podverse.ai';
+
+function userPrimaryEmailAddress(user: any): string {
+  return user.email_addresses.find((e: any) => e.id === user.primary_email_addressId)?.email_address || 'unknown';
+}
 
 /** Receives webhooks from Clerk when events of interest occur. */
 export async function POST(req: Request) {
@@ -21,7 +29,25 @@ export async function POST(req: Request) {
     'svix-id': svix_id,
     'svix-timestamp': svix_timestamp,
     'svix-signature': svix_signature,
-  });
+  }) as any;
   console.log(`Received Clerk webhook: ${JSON.stringify(payload, null, 2)}`);
+
+  if (payload.type === 'user.created') {
+    const userEmailAddress = userPrimaryEmailAddress(payload.data);
+    sendEmail({
+      to: ADMIN_EMAIL,
+      subject: 'New user created',
+      text: `A new user was created: ${userEmailAddress}`,
+    });
+  } else if (payload.type === 'session.created') {
+    const userId = payload.data.user_id;
+    const user = await clerkClient.users.getUser(userId);
+    const userEmailAddress = userPrimaryEmailAddress(user);
+    sendEmail({
+      to: ADMIN_EMAIL,
+      subject: 'User login',
+      text: `User logged in: ${userEmailAddress}`,
+    });
+  }
   return NextResponse.json({ ok: true });
 }
