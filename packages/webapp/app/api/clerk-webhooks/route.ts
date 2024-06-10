1040d4fa7e2975d68bad74867ce65828a625c7c6
Matt Welsh <matt@fixie.ai>
2024-06-10 13:11:24 -0700

    Adding webhook.

--- packages/webapp/app/api/clerk-webhooks/route.ts
+++ packages/webapp/app/api/clerk-webhooks/route.ts
@@ -1,13 +1,27 @@
 import Cors from 'micro-cors';
-//import { headers } from 'next/headers';
 import { NextResponse } from 'next/server';
+import { Webhook } from 'svix';
 
 const cors = Cors({
   allowMethods: ['POST', 'HEAD'],
 });
 
+/** Receives webhooks from Clerk when events of interest occur. */
 export async function POST(req: Request) {
+  const svix_id = req.headers.get('svix-id') ?? '';
+  const svix_timestamp = req.headers.get('svix-timestamp') ?? '';
+  const svix_signature = req.headers.get('svix-signature') ?? '';
+  const secret = process.env.CLERK_WEBHOOK_SECRET;
+  if (!secret) {
+    return NextResponse.json({ ok: false, error: 'Missing CLERK_WEBHOOK_SECRET' }, { status: 500 });
+  }
+  const wh = new Webhook(secret);
   const body = await req.text();
-  console.log(`Received Clerk webhook: ${body}`);
+  const payload = wh.verify(body, {
+    'svix-id': svix_id,
+    'svix-timestamp': svix_timestamp,
+    'svix-signature': svix_signature,
+  });
+  console.log(`Received Clerk webhook: ${payload}`);
   return NextResponse.json({ ok: true });
 }
