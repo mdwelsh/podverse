001b90e78cda8285a5ece7b75a8a50a24388947b
Matt Welsh <matt@fixie.ai>
2024-07-02 17:44:14 -0700

    Try removing extraneous url field.

--- packages/webapp/app/api/embed/route.ts
+++ packages/webapp/app/api/embed/route.ts
@@ -3,7 +3,6 @@ export async function GET(req: Request) {
   const response = {
     type: 'rich',
     version: '1.0',
-    url: 'https://www.podverse.ai/podcast/omnibus',
     //    html: '<iframe src="https://www.podverse.ai/embed/chat/omnibus" width="100%" height="600" frameborder="0" scrolling="no"></iframe>',
     html: '<div>This is a test from Podverse.</div>',
     width: 600,
