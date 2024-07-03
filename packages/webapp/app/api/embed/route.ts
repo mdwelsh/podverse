d9dd2cd4e3fdd5a4facc1eb971f0131ef157cf2f
Matt Welsh <matt@fixie.ai>
2024-07-02 17:21:49 -0700

    Include HTML in rich oEmbed response.

--- packages/webapp/app/api/embed/route.ts
+++ packages/webapp/app/api/embed/route.ts
@@ -3,7 +3,8 @@ export async function GET(req: Request) {
   const response = {
     type: 'rich',
     version: '1.0',
-    url: 'https://www.podverse.ai/embed/chat/omnibus',
+    url: 'https://www.podverse.ai/podcast/omnibus',
+    html: '<iframe src="https://www.podverse.ai/embed/chat/omnibus" width="100%" height="600" frameborder="0" scrolling="no"></iframe>',
   };
   return new Response(JSON.stringify(response), {
     headers: {
